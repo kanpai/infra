0a71f32c6197d9305f13d21cbbd7b889397bd5b8
mib <mib@kanp.ai>
2023-06-29 15:45:49 +0200

    feat(tools/build-iso): add tool to build bootstrapping isos

---
+++ bootstrap-iso.nix
@@ -0,0 +1,24 @@
+{ lib, ... }:
+let
+  config = import ./config.nix { inherit lib; };
+in
+{
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "yes";
+  };
+  users.users.root.openssh.authorizedKeys.keys = [
+    config.secrets.ssh.bootstrap
+  ];
+
+  networking = {
+    usePredictableInterfaceNames = lib.mkForce true;
+    hostName = "nixos";
+  };
+
+  isoImage.squashfsCompression = "lz4";
+}
