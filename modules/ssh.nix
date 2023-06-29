50c388f0a6e46822106b65c6dd50172b114bc1da
mib <mib@kanp.ai>
2023-06-29 22:06:25 +0200

    feat(modules/ssh): persist host keys

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,25 +1,11 @@
 { lib, config, ... }: {
-  environment.persistence = lib.attrs.optional config.environment.persistence.main.enable {
-    main.directories = [
-      "/etc/ssh/host_keys"
-    ];
-  };
-
   services.openssh = {
     enable = true;
-    hostKeys = [
-      {
-        bits = 4096;
-        path = "/etc/ssh/host_keys/rsa_key";
-        type = "rsa";
-      }
-      {
-        path = "/etc/ssh/host_keys/ed25519_key";
-        type = "ed25519";
-      }
-    ];
     settings = {
       PermitRootLogin = "prohibit-password";
     };
   };
+
+  # persist host private keys (otherwise server fingerprint changes every reboot)
+  environment.persistence.main.files = map (key: key.path) config.services.openssh.hostKeys;
 }
