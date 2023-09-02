cc647e3a3b5f8b36a2c96a5816a7aee9d3547add
mib <mib@mib.dev>
2023-09-02 15:21:04 +0200

    fix(modules/acme): use production acme api

--- modules/acme.nix
+++ modules/acme.nix
@@ -3,8 +3,7 @@
     acceptTerms = true;
     defaults = {
       email = "mib@mib.dev";
-      #server = "https://acme-v02.api.letsencrypt.org/directory";
-      server = "https://acme-staging-v02.api.letsencrypt.org/directory";
+      server = "https://acme-v02.api.letsencrypt.org/directory";
       # defaults to production; testing should be done with staging
       # https://acme-staging-v02.api.letsencrypt.org/directory
     };
