f0f4ad98f158c61bb59b1328ad4387d493de84e8
mib <mib@mib.dev>
2023-09-02 18:34:48 +0200

    fix(modules/acme): add default webroot

--- modules/acme.nix
+++ modules/acme.nix
@@ -3,6 +3,7 @@
     acceptTerms = true;
     defaults = {
       email = "mib@mib.dev";
+      webroot = "/var/lib/acme/acme-challenge";
       server = "https://acme-v02.api.letsencrypt.org/directory";
       # defaults to production; testing should be done with staging
       # https://acme-staging-v02.api.letsencrypt.org/directory
