81c46e803daf511547c2132871a142c695b32310
mib <mib@mib.dev>
2023-11-23 10:17:56 +0100

    fix(modules/postgresql): typo in attribute name

--- modules/postgresql.nix
+++ modules/postgresql.nix
@@ -3,7 +3,7 @@ let
   cfg = config.services.postgresql;
 in
 {
-  services.postgresql.enable = lib.mkDefault (cfg.ensureUsers != [ ] || cfg.ensureDatabase != [ ]);
+  services.postgresql.enable = lib.mkDefault (cfg.ensureUsers != [ ] || cfg.ensureDatabases != [ ]);
 
   persist.directories = lib.optional cfg.enable {
     directory = cfg.dataDir;
