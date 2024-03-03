b9a2fa96972585b9b81b95cd089acf127f3fa73c
mib <mib@mib.dev>
2024-03-03 10:36:07 +0100

    roles/vpn: persist tailscale data (wireguard key, etc)

--- modules/vpn.nix
+++ modules/vpn.nix
@@ -6,5 +6,13 @@
     extraUpFlags = [ "--login-server=https://headscale.kanp.ai" ];
   };
 
+  # should upstream a change that makes tailscale run as proper user with right permissions.
+  # it really shouldn't run as root...
+  persist.directories = [{
+    directory = "/var/lib/tailscale";
+    user = "root";
+    group = "root";
+  }];
+
   age.secrets.vpn-preauth.file = ../secrets/vpn-preauth.age;
 }
