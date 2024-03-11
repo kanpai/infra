22e93a9441ee1d0a6439bc7cfd709b3415128213
mib <mib@mib.dev>
2024-03-11 21:22:24 +0100

    lib: mechanism to allow insecure packages conditionally

--- lib.nix
+++ lib.nix
@@ -23,6 +23,10 @@ let
       _module.args =
         let
           inherit (nixpkgs-lib.strings) hasPrefix removePrefix;
+
+          # "release" = [ "pkgs-0.1.0" "to-0.2.0" "allow-0.3.0];
+          # i.e. `"23.11" = [ "hello-2.12.1" ];`
+          permittedInsecurePackages = { };
         in
         rec {
           inherit inputs args;
@@ -33,7 +37,19 @@ let
         } // nixpkgs-lib.attrsets.foldlAttrs
           (acc: name: input: acc // nixpkgs-lib.optionalAttrs
             (hasPrefix "nixpkgs-" name)
-            { ${removePrefix "nix" name} = input.legacyPackages.${module.system}; }
+            {
+              ${removePrefix "nix" name} =
+                let
+                  version = removePrefix "nixpkgs-" name;
+                in
+                if !permittedInsecurePackages ? ${version}
+                then input.legacyPackages.${module.system}
+                else
+                  import input {
+                    inherit (module) system;
+                    config.permittedInsecurePackages = permittedInsecurePackages.${version};
+                  };
+            }
           )
           { }
           inputs;
