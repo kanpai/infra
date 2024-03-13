61d3cb8361274209881466a6a66dcf6c1ca2bbe1
mib <mib@mib.dev>
2024-03-13 17:08:02 +0100

    roles/kanna/mail: migrate to postgres

--- roles/kanna/mail.nix
+++ roles/kanna/mail.nix
@@ -3,6 +3,8 @@ let
   domain = "kanp.ai";
   fqdn = "mail.${domain}";
 
+  dbname = "maddy";
+
   cfg = config.services.maddy;
 in
 {
@@ -25,134 +27,143 @@ in
           }];
       };
 
-      config = ''
-        auth.pass_table local_authdb {
-          table sql_table {
-            driver sqlite3
-            dsn database.db
-            table_name passwords
-          }
-        }
-
-        storage.imapsql local_mailboxes {
-          driver sqlite3
-          dsn database.db
-        }
-
-        table.chain local_rewrites {
-          optional_step regexp "(.+)\+(.+)@(.+)" "$1@$3"
-          optional_step static {
-            entry postmaster postmaster@$(primary_domain)
-          }
-          optional_step file /etc/maddy/aliases
-        }
-
-        msgpipeline local_routing {
-          destination postmaster $(local_domains) {
-            modify {
-              replace_rcpt &local_rewrites
+      config =
+        let
+          database = ''
+            driver postgres
+            dsn postgres://${dbname}@/${dbname}?host=/run/postgresql
+          '';
+        in
+        ''
+          auth.pass_table local_authdb {
+            table sql_table {
+              ${database}
+              table_name passwords
             }
-            deliver_to &local_mailboxes
           }
-          default_destination {
-            reject 550 5.1.1 "User doesn't exist"
-          }
-        }
 
-        smtp tcp://0.0.0.0:25 {
-          limits {
-            all rate 20 1s
-            all concurrency 10
+          storage.imapsql local_mailboxes {
+            ${database}
           }
-          dmarc yes
-          check {
-            require_mx_record
-            dkim
-            spf
+
+          table.chain local_rewrites {
+            optional_step regexp "(.+)\+(.+)@(.+)" "$1@$3"
+            optional_step static {
+              entry postmaster postmaster@$(primary_domain)
+            }
+            optional_step file /etc/maddy/aliases
           }
-          source $(local_domains) {
-            reject 501 5.1.8 "Use Submission for outgoing SMTP"
-          }
-          default_source {
+
+          msgpipeline local_routing {
             destination postmaster $(local_domains) {
-              deliver_to &local_routing
+              modify {
+                replace_rcpt &local_rewrites
+              }
+              deliver_to &local_mailboxes
             }
             default_destination {
               reject 550 5.1.1 "User doesn't exist"
             }
           }
-        }
 
-        submission tcp://0.0.0.0:587 {
-          limits {
-            all rate 50 1s
-          }
-          auth &local_authdb
-          source $(local_domains) {
+          smtp tcp://0.0.0.0:25 {
+            limits {
+              all rate 20 1s
+              all concurrency 10
+            }
+            dmarc yes
             check {
-                authorize_sender {
-                    prepare_email &local_rewrites
-                    user_to_email identity
-                }
+              require_mx_record
+              dkim
+              spf
             }
-            destination postmaster $(local_domains) {
+            source $(local_domains) {
+              reject 501 5.1.8 "Use Submission for outgoing SMTP"
+            }
+            default_source {
+              destination postmaster $(local_domains) {
                 deliver_to &local_routing
-            }
-            default_destination {
-                modify {
-                    dkim $(primary_domain) $(local_domains) default
-                }
-                deliver_to &remote_queue
+              }
+              default_destination {
+                reject 550 5.1.1 "User doesn't exist"
+              }
             }
           }
-          default_source {
-            reject 501 5.1.8 "Non-local sender domain"
-          }
-        }
 
-        target.remote outbound_delivery {
-          limits {
-            destination rate 20 1s
-            destination concurrency 10
-          }
-          mx_auth {
-            dane
-            mtasts {
-              cache fs
-              fs_dir mtasts_cache/
+          submission tcp://0.0.0.0:587 {
+            limits {
+              all rate 50 1s
             }
-            local_policy {
-                min_tls_level encrypted
-                min_mx_level none
+            auth &local_authdb
+            source $(local_domains) {
+              check {
+                  authorize_sender {
+                      prepare_email &local_rewrites
+                      user_to_email identity
+                  }
+              }
+              destination postmaster $(local_domains) {
+                  deliver_to &local_routing
+              }
+              default_destination {
+                  modify {
+                      dkim $(primary_domain) $(local_domains) default
+                  }
+                  deliver_to &remote_queue
+              }
+            }
+            default_source {
+              reject 501 5.1.8 "Non-local sender domain"
             }
           }
-        }
 
-        target.queue remote_queue {
-          target &outbound_delivery
-          autogenerated_msg_domain $(primary_domain)
-          bounce {
-            destination postmaster $(local_domains) {
-              deliver_to &local_routing
+          target.remote outbound_delivery {
+            limits {
+              destination rate 20 1s
+              destination concurrency 10
             }
-            default_destination {
-                reject 550 5.0.0 "Refusing to send DSNs to non-local addresses"
+            mx_auth {
+              dane
+              mtasts {
+                cache fs
+                fs_dir mtasts_cache/
+              }
+              local_policy {
+                  min_tls_level encrypted
+                  min_mx_level none
+              }
             }
           }
-        }
 
-        imap tcp://0.0.0.0:143 {
-          auth &local_authdb
-          storage &local_mailboxes
-        }
-      '';
+          target.queue remote_queue {
+            target &outbound_delivery
+            autogenerated_msg_domain $(primary_domain)
+            bounce {
+              destination postmaster $(local_domains) {
+                deliver_to &local_routing
+              }
+              default_destination {
+                  reject 550 5.0.0 "Refusing to send DSNs to non-local addresses"
+              }
+            }
+          }
+
+          imap tcp://0.0.0.0:143 {
+            auth &local_authdb
+            storage &local_mailboxes
+          }
+        '';
     };
 
     postgresql = {
-      ensureDatabases = [ "maddy" ];
-      ensureUsers = [{ name = "maddy"; ensureDBOwnership = true; }];
+      ensureDatabases = [ dbname ];
+      ensureUsers = [{ name = dbname; ensureDBOwnership = true; }];
+      # not entirely sure peer auth fails, but this works for now.
+      authentication = "local maddy maddy trust";
     };
   };
+
+  systemd.services.maddy.requires = [ "postgresql.service" ];
 
   security.acme.certs.${fqdn} = {
     group = cfg.group;
