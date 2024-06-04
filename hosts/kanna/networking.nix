2388ec8f633eb5ba689b2e558cde35a017dac3d2
mib <mib@mib.dev>
2024-06-04 14:21:46 +0200

    hosts/kanna/networking: enable both ethernet interfaces

--- hosts/kanna/networking.nix
+++ hosts/kanna/networking.nix
@@ -1,25 +1,29 @@
-{ host, ... }:
+{ lib, host, ... }:
 let
-  networks = {
-    "10-lan" = {
-      matchConfig.Name = "eno1";
-      networkConfig = {
-        DHCP = "ipv4";
-        IPv6AcceptRA = true;
-      };
-      dns = [
-        "9.9.9.9"
-        "149.112.112.112"
-        "2620:fe::fe"
-        "2620:fe::9"
-
-        "1.1.1.1"
-        "1.0.0.1"
-        "2606:4700:4700::1111"
-        "2606:4700:4700::1001"
-      ];
+  mkLan = interface: {
+    matchConfig.Name = interface;
+    networkConfig = {
+      DHCP = "ipv4";
+      IPv6AcceptRA = true;
     };
+    dns = [
+      "9.9.9.9"
+      "149.112.112.112"
+      "2620:fe::fe"
+      "2620:fe::9"
+
+      "1.1.1.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+    ];
   };
+  networks = lib.listToAttrs (map
+    (interface: {
+      name = "10-lan-${interface}";
+      value = mkLan interface;
+    })
+    [ "eno1" "eno2" ]);
 in
 {
   systemd.network = {
