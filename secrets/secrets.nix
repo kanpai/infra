5ffa38bb9ca65d65e32e95d61a8fe78f42df95d8
mib <mib@mib.dev>
2024-06-09 00:43:01 +0200

    secrets: fine-grained configuration of secrets
    allows specifying e.g. which admins and machines can read the secret.
    also overriding the filename of secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,22 +2,27 @@ let
   lib = (import <nixpkgs/lib>) // (import ../lib.nix { inputs = { }; });
   config = import ../config.nix { inherit lib; };
 
-  inherit (lib.attrsets) collect recursiveUpdate;
-  inherit (lib.lists) foldl foldr;
-  inherit (builtins) typeOf mapAttrs;
+  inherit (lib.attrsets) collect getAttrFromPath recursiveUpdate;
+  inherit (lib.strings) splitString;
+  inherit (lib.lists) flatten foldl foldr;
+  inherit (builtins) mapAttrs;
 
   getKeys = m: (m.keys.ssh or [ ]) ++ (m.keys.age or [ ]);
-  foldKeys = foldl (acc: m: acc ++ getKeys m) [ ];
-  keys = foldKeys config.admins ++ foldKeys (collect (c: c ? name) config.machines);
+  intoKeys = configs: flatten (map getKeys configs);
+  keysFromPath = type: path: getAttrFromPath (splitString "/" path) config.${type};
+  collectKeys = set: collect (m: m ? "name") set;
 
   traverse =
     let
       sanitize = lib.replaceStrings [ "-" ] [ "_" ];
+      isEndpoint = set: set == { } || set ? "admins" || set ? "machines";
     in
     set: names: mapAttrs
       (name: value:
-        if value == { }
-        then foldr (acc: name: "${acc}-${name}") (sanitize name) names + ".age"
+        if isEndpoint value
+        then value // {
+          filename = foldr (acc: name: "${acc}-${name}") (sanitize name) names + ".age";
+        }
         else traverse value (names ++ [ (sanitize name) ])
       )
       set;
@@ -40,14 +45,21 @@ let
       "mewo"
       "muffin"
     ]
-      (_: { private-key = { }; });
+      (host: {
+        private-key.machines = [ host ];
+      });
   };
 in
-foldl
-  recursiveUpdate
-{ }
+foldl recursiveUpdate { }
   (map
-    (filename: { ${filename} = { publicKeys = keys; }; })
+    (s: {
+      ${s.filename} = {
+        publicKeys = lib.flatten (map intoKeys [
+          (if s ? "admins" then map (keysFromPath "admins") s.admins else collectKeys config.admins)
+          (if s ? "machines" then map (keysFromPath "machines") s.machines else collectKeys config.machines)
+        ]);
+      };
+    })
     (collect
-      (secret: typeOf secret == "string")
+      (secret: secret ? filename)
       (traverse secrets [ ])))
