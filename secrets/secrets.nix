b8e08cedfe673f03878c0fcc38776a101297af22
mib <mib@mib.dev>
2024-06-09 11:02:50 +0200

    secrets: simplify with library functions

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,30 +2,17 @@ let
   lib = (import <nixpkgs/lib>) // (import ../lib.nix { inputs = { }; });
   config = import ../config.nix { inherit lib; };
 
-  inherit (lib.attrsets) collect getAttrFromPath recursiveUpdate;
-  inherit (lib.strings) splitString;
-  inherit (lib.lists) flatten foldl foldr;
-  inherit (builtins) mapAttrs;
+  inherit (lib.attrsets) collect getAttrFromPath listToAttrs mapAttrsRecursiveCond;
+  inherit (lib.strings) concatMapStringsSep splitString;
+  inherit (lib.lists) flatten;
 
   getKeys = m: (m.keys.ssh or [ ]) ++ (m.keys.age or [ ]);
   intoKeys = configs: flatten (map getKeys configs);
   keysFromPath = type: path: getAttrFromPath (splitString "/" path) config.${type};
   collectKeys = set: collect (m: m ? "name") set;
 
-  traverse =
-    let
-      sanitize = lib.replaceStrings [ "-" ] [ "_" ];
-      isEndpoint = set: set == { } || set ? "admins" || set ? "machines";
-    in
-    set: names: mapAttrs
-      (name: value:
-        if isEndpoint value
-        then value // {
-          filename = foldr (acc: name: "${acc}-${name}") (sanitize name) names + ".age";
-        }
-        else traverse value (names ++ [ (sanitize name) ])
-      )
-      set;
+  sanitize = lib.replaceStrings [ "-" ] [ "_" ];
+  isEndpoint = set: set == { } || set ? "admins" || set ? "machines";
 
   secrets = {
     i2p = {
@@ -50,16 +37,18 @@ let
       });
   };
 in
-foldl recursiveUpdate { }
-  (map
-    (s: {
-      ${s.filename} = {
-        publicKeys = lib.flatten (map intoKeys [
+listToAttrs
+  (collect
+    (s: s ? "name")
+    (mapAttrsRecursiveCond
+      (s: !(isEndpoint s))
+      (path: s: {
+        name = concatMapStringsSep "-" sanitize path + ".age";
+        value.publicKeys = flatten (map intoKeys [
           (if s ? "admins" then map (keysFromPath "admins") s.admins else collectKeys config.admins)
           (if s ? "machines" then map (keysFromPath "machines") s.machines else collectKeys config.machines)
         ]);
-      };
-    })
-    (collect
-      (secret: secret ? filename)
-      (traverse secrets [ ])))
+      })
+      secrets
+    )
+  )
