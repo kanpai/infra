e8fa5cece487c4b7aee8c9348e4a9fd0b4964bd0
mib <mib@mib.dev>
2024-06-09 14:16:05 +0200

    flake: update latest nixpkgs release to 24.05

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
   inputs = {
     nixpkgs.follows = "nixpkgs-unstable";
-    nixpkgs-stable.follows = "nixpkgs-23_11";
+    nixpkgs-stable.follows = "nixpkgs-24_05";
 
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    nixpkgs-24_05.url = "nixpkgs/nixos-24.05";
     nixpkgs-23_11.url = "nixpkgs/nixos-23.11";
     nixpkgs-23_05.url = "nixpkgs/nixos-23.05";
 
